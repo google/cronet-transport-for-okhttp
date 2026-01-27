7fbf3d1beb6254294e6f1158898d564545b8a68d
Vladyslav Kasprov <vkasprov@google.com>
2026-01-27 10:30:35 -0800

    Add large read benchmarks that simulate response parsing
    This change enhances the existing `LargeReadBenchmarkTest`. Currently, its benchmarks don't do anything when reading the response, immediately discarding the bytes. To simulate realistic scenarios this change adds benchmarks that do CPU intensive work on read, which is intended to simulate response parsing (20 microseconds on each read).
    
    PiperOrigin-RevId: 861784934

--- javatests/com/google/net/cronet/okhttptransport/LargeReadBenchmarkTest.java
+++ javatests/com/google/net/cronet/okhttptransport/LargeReadBenchmarkTest.java
@@ -20,6 +20,9 @@ import static com.google.common.truth.Truth.assertThat;
 
 import android.util.Log;
 import com.google.net.cronet.testing.CronetEngineTestAppRule;
+import java.io.IOException;
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Locale;
@@ -31,7 +34,8 @@ import okhttp3.Response;
 import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.MockWebServer;
 import okio.Buffer;
-import okio.Okio;
+import okio.Sink;
+import okio.Timeout;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -63,6 +67,8 @@ import org.junit.runners.Parameterized.Parameters;
  * <p>At the time of writing, this benchmark was used to measure the performance impact of changing
  * bridge buffer when reading the response body from Cronet, see <a
  * href="https://github.com/google/cronet-transport-for-okhttp/issues/47">google/cronet-transport-for-okhttp#47</a>.
+ *
+ * <p>This benchmark must be run on API 33+ devices because it uses {@link Thread#onSpinWait()}.
  */
 @RunWith(Parameterized.class)
 public class LargeReadBenchmarkTest {
@@ -77,23 +83,40 @@ public class LargeReadBenchmarkTest {
   @Parameter(1)
   public int responseSizeMb;
 
+  /**
+   * Every time OkHttp completes a read, we will spin loop for this amount of time. This can be used
+   * to simulate work being done on the OkHttp caller threads in between reads.
+   */
+  @Parameter(2)
+  public Duration workDuration;
+
   @Parameters(name = "{0}")
   public static Collection<Object[]> parameters() {
     return Arrays.asList(
         new Object[][] {
-          {" WarmupIteration#01", 10},
-          {" WarmupIteration#02", 10},
-          {" WarmupIteration#03", 10},
-          {"Iteration#01", 50},
-          {"Iteration#02", 50},
-          {"Iteration#03", 50},
-          {"Iteration#04", 50},
-          {"Iteration#05", 50},
-          {"Iteration#06", 50},
-          {"Iteration#07", 50},
-          {"Iteration#08", 50},
-          {"Iteration#09", 50},
-          {"Iteration#10", 50},
+          {"   Warmup#01", 10, Duration.ZERO},
+          {"   Warmup#02", 10, Duration.ZERO},
+          {"   Warmup#03", 10, Duration.ZERO},
+          {"Iteration#01", 50, Duration.ZERO},
+          {"Iteration#02", 50, Duration.ZERO},
+          {"Iteration#03", 50, Duration.ZERO},
+          {"Iteration#04", 50, Duration.ZERO},
+          {"Iteration#05", 50, Duration.ZERO},
+          {"Iteration#06", 50, Duration.ZERO},
+          {"Iteration#07", 50, Duration.ZERO},
+          {"Iteration#08", 50, Duration.ZERO},
+          {"Iteration#09", 50, Duration.ZERO},
+          {"Iteration#10", 50, Duration.ZERO},
+          {"Iteration#11", 50, Duration.of(20, ChronoUnit.MICROS)},
+          {"Iteration#12", 50, Duration.of(20, ChronoUnit.MICROS)},
+          {"Iteration#13", 50, Duration.of(20, ChronoUnit.MICROS)},
+          {"Iteration#14", 50, Duration.of(20, ChronoUnit.MICROS)},
+          {"Iteration#15", 50, Duration.of(20, ChronoUnit.MICROS)},
+          {"Iteration#16", 50, Duration.of(20, ChronoUnit.MICROS)},
+          {"Iteration#17", 50, Duration.of(20, ChronoUnit.MICROS)},
+          {"Iteration#18", 50, Duration.of(20, ChronoUnit.MICROS)},
+          {"Iteration#19", 50, Duration.of(20, ChronoUnit.MICROS)},
+          {"Iteration#20", 50, Duration.of(20, ChronoUnit.MICROS)},
         });
   }
 
@@ -131,12 +154,13 @@ public class LargeReadBenchmarkTest {
     Request request = new Request.Builder().url(server.url("/")).build();
     Call call = callFactory.newCall(request);
     Response response = call.execute();
+    BlackholeSinkWithWork sink = new BlackholeSinkWithWork();
 
     long readStartNs = System.nanoTime();
-    long bytesRead = response.body().source().readAll(Okio.blackhole());
+    long bytesRead = response.body().source().readAll(sink);
     long readEndNs = System.nanoTime();
 
-    logResults(testName, bytesRead, readStartNs, readEndNs);
+    logResults(testName, sink.readsCount, readStartNs, readEndNs);
 
     assertThat(response.code()).isEqualTo(200);
     assertThat(bytesRead).isEqualTo(responseSizeBytes);
@@ -159,16 +183,65 @@ public class LargeReadBenchmarkTest {
     return buffer;
   }
 
-  private void logResults(String testName, long bytesRead, long readStartNs, long readEndNs) {
+  private void logResults(String testName, int readsCount, long readStartNs, long readEndNs) {
     long readDurationMs = (readEndNs - readStartNs) / 1_000_000L;
+
+    String workDurationString =
+        workDuration.isPositive()
+            ? String.format("work duration: %dus", workDuration.toNanos() / 1_000L)
+            : "no work";
+
     Log.e(
         "LargeReadBenchmarkTest",
         String.format(
             Locale.US,
-            "%s - %s - bytes read: %d, read duration: %dms",
+            "%s - %s - response size: %dMB, reads count: %d, %s, read duration: %dms",
             testName,
             iterationName,
-            bytesRead,
+            responseSizeMb,
+            readsCount,
+            workDurationString,
             readDurationMs));
+  }
+
+  /**
+   * A sink that does CPU intensive work on each read, which is intended to simulate response
+   * parsing by a consumer application. On each read, it first discards all bytes written to it and
+   * then occupies the CPU for {@link #workDuration}.
+   */
+  private final class BlackholeSinkWithWork implements Sink {
+
+    private int readsCount = 0;
+
+    @Override
+    public void write(Buffer source, long byteCount) throws IOException {
+      source.skip(byteCount);
+      readsCount++;
+      if (workDuration.isPositive()) {
+        doWork();
+      }
+    }
+
+    private void doWork() {
+      long startNs = System.nanoTime();
+      while (System.nanoTime() - startNs < workDuration.toNanos()) {
+        Thread.onSpinWait();
+      }
+    }
+
+    @Override
+    public void flush() throws IOException {
+      // Do nothing
+    }
+
+    @Override
+    public Timeout timeout() {
+      return Timeout.NONE;
+    }
+
+    @Override
+    public void close() throws IOException {
+      // Do nothing
+    }
   }
 }
